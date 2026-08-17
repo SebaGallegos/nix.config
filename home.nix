15f6f64eafef4fa42031291db28f215351273cb1
Sebastián Gallegos F. <59512190+SebaGallegos@users.noreply.github.com>
2026-08-17 17:17:56 -0400

    update
    Reorder packages and update comments in home.nix.

--- home.nix
+++ home.nix
@@ -9,27 +9,27 @@
     ./modules/user.config/starship.nix
   ];
 
-  # WARNING
-  # DO NOT CHANGE THIS!!!
   home.stateVersion = "26.05";
 
   home.packages = [
-    pkgs.git
-    pkgs.fastfetch
     pkgs.brave
-    pkgs.pinentry-qt
-
+    pkgs.fastfetch
+    pkgs.git
     pkgs.nerd-fonts.fira-code
+    pkgs.pinentry-qt
+    pkgs.prismlauncher
+    pkgs.vlc
+
     inputs.helium.packages.${pkgs.system}.default
   ];
 
   programs.bash = {
     enable = true;
     shellAliases = {
-      # to create a new generation
+      # para hacer una nueva generación
       sys-update = "sudo nixos-rebuild switch --flake ~/.dotfiles#nixos";
 
-      # to wipe all old generations (only keeps the actual) and wipe their entries on systemd-boot
+      # para limpiar todo y actualizar systemd-boot
       sys-clean = "echo 'Warning: This wipes all old generations.' && read -p 'Press Enter to continue or Ctrl+C to cancel...' && sudo nix-collect-garbage -d && sudo nixos-rebuild boot --flake ~/.dotfiles#nixos";
     };
   };
