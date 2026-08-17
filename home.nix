04e5765817f87138140e361253191434f86dc11d
Sebastián Gallegos F. <59512190+SebaGallegos@users.noreply.github.com>
2026-08-17 14:53:00 -0400

    Update comments in home.nix

--- home.nix
+++ home.nix
@@ -6,8 +6,11 @@
 
   imports = [
     ./modules/user.config/git.config.nix
+    ./modules/user.config/starship.nix
   ];
 
+  # WARNING
+  # DO NOT CHANGE THIS!!!
   home.stateVersion = "26.05";
 
   home.packages = [
@@ -15,16 +18,18 @@
     pkgs.fastfetch
     pkgs.brave
     pkgs.pinentry-qt
+
+    pkgs.nerd-fonts.fira-code
     inputs.helium.packages.${pkgs.system}.default
   ];
 
   programs.bash = {
     enable = true;
     shellAliases = {
-      # para hacer una nueva generación
+      # to create a new generation
       sys-update = "sudo nixos-rebuild switch --flake ~/.dotfiles#nixos";
 
-      # para limpiar todo y actualizar systemd-boot
+      # to wipe all old generations (only keeps the actual) and wipe their entries on systemd-boot
       sys-clean = "echo 'Warning: This wipes all old generations.' && read -p 'Press Enter to continue or Ctrl+C to cancel...' && sudo nix-collect-garbage -d && sudo nixos-rebuild boot --flake ~/.dotfiles#nixos";
     };
   };
